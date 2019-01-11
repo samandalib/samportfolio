6527029adb0570bb9c36f7423a65f4127527397a
samandalib <hesam.andalib@gmail.com>
2019-01-11 20:57:43 +0330

    modification of the css files and starting the project2.html

--- docs/js/navbar.js
+++ docs/js/navbar.js
@@ -1,12 +1,9 @@
-$(document).ready(function() {
-    $('a[href="#navbar-more-show"], .navbar-more-overlay').on('click', function(event) {
-		event.preventDefault();
-		$('body').toggleClass('navbar-more-show');
-		if ($('body').hasClass('navbar-more-show'))	{
-			$('a[href="#navbar-more-show"]').closest('li').addClass('active');
-		}else{
-			$('a[href="#navbar-more-show"]').closest('li').removeClass('active');
-		}
-		return false;
-	});
-});
+/* Navigation Responsive*/
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav nav-pills nav-fill") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav nav-pills nav-fill";
+  }
+}
